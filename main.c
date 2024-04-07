39c92fe244b244abba7a57a1a6edaf1b77479811
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-08 01:34:14 +0530

    chore: InputBuffer function & print

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 int main() {
     // print prompt
+    print_prompt();
+
     // get input
+    
 
     // read commands
         // check if command is valid or not
@@ -13,3 +16,16 @@ typedef struct {
     size_t buffer_length;
     ssize_t input_length;
 } InputBuffer;
+
+InputBuffer* new_input_buffer() {
+  InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+
+  return input_buffer;
+}
+
+void print_prompt() { 
+    printf("db > "); 
+}
